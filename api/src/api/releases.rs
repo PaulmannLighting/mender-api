019c972b08fee060ae0abe83ec03c28c6e0b06e6
Richard Neumann <neumann@paulmann.de>
2025-08-04 23:03:50 +0200

    Return deserialized JSON objects.

--- api/src/api/releases.rs
+++ api/src/api/releases.rs
@@ -1,3 +1,4 @@
+use crate::api::dto::ReleaseList;
 use crate::api::session::Session;
 
 const PATH: &str = "/api/management/v1/deployments/deployments/releases/list";
@@ -5,18 +6,18 @@ const PATH: &str = "/api/management/v1/deployments/deployments/releases/list";
 /// Trait to manage releases in the mender server API.
 pub trait Releases {
     /// List all releases available in the Mender server.
-    fn list(&self) -> impl Future<Output = reqwest::Result<String>>;
+    fn list(&self) -> impl Future<Output = reqwest::Result<ReleaseList>>;
 }
 
 impl Releases for Session {
-    async fn list(&self) -> reqwest::Result<String> {
+    async fn list(&self) -> reqwest::Result<ReleaseList> {
         self.client()
             .get(self.url(PATH))
             .bearer_auth(self.bearer_token())
             .send()
             .await?
             .error_for_status()?
-            .text()
+            .json()
             .await
     }
 }
