2827c1df8df4dc84e81a23fa5398d07f8807fd08
Richard Neumann <neumann@paulmann.de>
2025-08-04 20:34:28 +0200

    Update API

--- api/src/api.rs
+++ api/src/api.rs
@@ -1,18 +1,14 @@
 //!  Implementation of the Mender server API.
 
-pub use api_version::ApiVersion;
 pub use deployments::Deployments;
 pub use devauth::Devauth;
-pub use inventory::Inventory;
-pub use management::Management;
+pub use devices::{DeviceList, Devices};
+pub use login::Login;
 pub use mender_server::MenderServer;
-pub use user_adm::UserAdm;
 
-mod api_version;
 mod deployments;
 mod devauth;
-mod endpoint;
-mod inventory;
-mod management;
+mod devices;
+mod login;
 mod mender_server;
-mod user_adm;
+mod session;
--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,6 +1,6 @@
 //! API to the mender server extensions.
 
-pub use api::{ApiVersion, Deployments, Devauth, Inventory, Management, MenderServer, UserAdm};
+pub use api::{Deployments, Devauth, DeviceList, Devices, Login, MenderServer};
 pub use reqwest::Certificate;
 
 mod api;
