844f4601d11f706f9dabe85bff521f1632a90724
Richard Neumann <neumann@paulmann.de>
2025-08-04 21:46:41 +0200

    Implement releases API.

--- api/src/api.rs
+++ api/src/api.rs
@@ -3,8 +3,10 @@
 pub use devices::{DeviceList, Devices};
 pub use login::Login;
 pub use mender_server::MenderServer;
+pub use releases::Releases;
 
 pub mod devices;
 mod login;
 mod mender_server;
+mod releases;
 mod session;
---
+++ api/src/api/releases.rs
@@ -0,0 +1,22 @@
+use crate::api::session::Session;
+
+const PATH: &str = "/api/management/v1/deployments/deployments/releases/list";
+
+/// Trait to manage releases in the mender server API.
+pub trait Releases {
+    /// List all releases available in the Mender server.
+    fn list(&self) -> impl Future<Output = reqwest::Result<String>>;
+}
+
+impl Releases for Session {
+    async fn list(&self) -> reqwest::Result<String> {
+        self.client()
+            .get(self.url(PATH))
+            .bearer_auth(self.bearer_token())
+            .send()
+            .await?
+            .error_for_status()?
+            .text()
+            .await
+    }
+}
