bc539ead4b2ff71a4cbfadbe518410557c4cef3e
Richard Neumann <neumann@paulmann.de>
2025-08-04 17:20:35 +0200

    Make API more generic.

--- api/src/api/user_adm.rs
+++ api/src/api/user_adm.rs
@@ -8,12 +8,20 @@ const LOGIN_ENDPOINT: &str = "auth/login";
 
 /// User administration API.
 pub trait UserAdm {
-    async fn login(&self, user_name: &str, password: &str) -> Result<Vec<u8>, reqwest::Error>;
+    async fn login(
+        &self,
+        user_name: impl AsRef<str>,
+        password: impl AsRef<str>,
+    ) -> Result<Vec<u8>, reqwest::Error>;
 }
 
 impl UserAdm for Endpoint<'_> {
-    async fn login(&self, user_name: &str, password: &str) -> Result<Vec<u8>, reqwest::Error> {
-        let authentication = Request::new(user_name, password);
+    async fn login(
+        &self,
+        user_name: impl AsRef<str>,
+        password: impl AsRef<str>,
+    ) -> Result<Vec<u8>, reqwest::Error> {
+        let authentication = Request::new(user_name.as_ref(), password.as_ref());
         let url = self.url().with_joined_path(LOGIN_ENDPOINT);
         let response = self
             .server
