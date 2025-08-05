081c321bccd502cc1a2ba9194bf6559578f7df68
Richard Neumann <neumann@paulmann.de>
2025-08-05 12:33:26 +0200

    Fix deployment status parsing.

--- api/src/api/dto/deployments/status.rs
+++ api/src/api/dto/deployments/status.rs
@@ -4,6 +4,8 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, Deserialize, Serialize)]
 #[serde(rename_all = "lowercase")]
 pub enum Status {
+    /// Deployment is pending.
+    Pending,
     /// Deployment is in progress.
     InProgress,
     /// Deployment has finished.
