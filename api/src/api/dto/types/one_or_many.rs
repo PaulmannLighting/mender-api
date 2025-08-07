e23f61fdce6a13cfb24760ff71d86dec1dfc5d60
Richard Neumann <neumann@paulmann.de>
2025-08-07 11:27:29 +0200

    Refactor formatting.

--- api/src/api/dto/types/one_or_many.rs
+++ api/src/api/dto/types/one_or_many.rs
@@ -56,7 +56,7 @@ where
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::One(item) => write!(f, "{}", item),
+            Self::One(item) => write!(f, "{item}"),
             Self::Many(items) => {
                 write!(f, "[")?;
 
