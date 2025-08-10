fd98b314ab307dfd7f3e10acb703ab2377197c7a
Richard Neumann <neumann@paulmann.de>
2025-08-10 11:42:26 +0200

    Remove device authentication API.
    It's currently unneeded.

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -2,7 +2,6 @@
 
 pub use client::Client;
 pub use deployments::Deployments;
-pub use dev_auth::DevAuth;
 pub use devices::Devices;
 pub use groups::Groups;
 pub use login::Login;
@@ -11,7 +10,6 @@ pub use reqwest::Certificate;
 
 mod client;
 mod deployments;
-mod dev_auth;
 mod devices;
 pub mod dto;
 mod groups;
