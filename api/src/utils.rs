9d3c0d41390012c674b8c16e4d98cc336b7170c2
Richard Neumann <neumann@paulmann.de>
2025-09-01 12:10:23 +0200

    Refactor `display_slice()`.

--- api/src/utils.rs
+++ api/src/utils.rs
@@ -5,18 +5,21 @@ use std::fmt::Display;
 pub mod as_str;
 
 /// Display a slice of items, formatting them as a comma-separated list enclosed in square brackets.
-pub fn display_slice<T>(items: &[T], f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
+pub fn display_slice<T>(slice: &[T], f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
 where
     T: Display,
 {
     write!(f, "[")?;
 
-    for (i, item) in items.iter().enumerate() {
-        if i > 0 {
-            write!(f, ", ")?;
-        }
+    let mut items = slice.iter();
 
-        item.fmt(f)?;
+    if let Some(first) = items.next() {
+        first.fmt(f)?;
+
+        for item in items {
+            write!(f, ", ")?;
+            item.fmt(f)?;
+        }
     }
 
     write!(f, "]")
