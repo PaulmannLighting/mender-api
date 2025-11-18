40bfcc7084f005c0dc62e57f1232b15cc3c0cf04
Richard Neumann <neumann@paulmann.de>
2025-11-18 09:38:21 +0100

    Fix artifacts listing API.

--- api/src/artifacts.rs
+++ api/src/artifacts.rs
@@ -6,7 +6,7 @@ use crate::dto::Artifact;
 use crate::pagination::DEFAULT_PAGE_SIZE;
 use crate::{Pager, PaginatedIterator, Session};
 
-const PATH_V2: &str = "/api/management/v2/deployments";
+const PATH: &str = "api/management/v1/deployments";
 
 /// Artifacts management API.
 pub trait Artifacts {
@@ -18,7 +18,7 @@ impl Artifacts for Session {
     fn list(&self, page_size: Option<NonZero<usize>>) -> PaginatedIterator<'_, '_, Artifact> {
         Pager::new(
             self,
-            format!("{PATH_V2}/artifacts").into(),
+            format!("{PATH}/artifacts/list").into(),
             page_size.unwrap_or(DEFAULT_PAGE_SIZE),
         )
         .into()
