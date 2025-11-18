67ec7aef390c44fb8602803d510f25743e234b90
Richard Neumann <neumann@paulmann.de>
2025-11-18 09:43:10 +0100

    Implement artifact deletion.

--- api/src/artifacts.rs
+++ api/src/artifacts.rs
@@ -2,25 +2,41 @@
 
 use std::num::NonZero;
 
+use uuid::Uuid;
+
 use crate::dto::Artifact;
 use crate::pagination::DEFAULT_PAGE_SIZE;
 use crate::{Pager, PaginatedIterator, Session};
 
-const PATH: &str = "api/management/v1/deployments";
+const PATH: &str = "api/management/v1/deployments/artifacts";
 
 /// Artifacts management API.
 pub trait Artifacts {
     /// List all artifacts.
     fn list(&self, page_size: Option<NonZero<usize>>) -> PaginatedIterator<'_, '_, Artifact>;
+
+    /// Delete an artifact by its ID.
+    fn delete(&self, id: Uuid) -> impl Future<Output = reqwest::Result<()>>;
 }
 
 impl Artifacts for Session {
     fn list(&self, page_size: Option<NonZero<usize>>) -> PaginatedIterator<'_, '_, Artifact> {
         Pager::new(
             self,
-            format!("{PATH}/artifacts/list").into(),
+            format!("{PATH}/list").into(),
             page_size.unwrap_or(DEFAULT_PAGE_SIZE),
         )
         .into()
+    }
+
+    async fn delete(&self, id: Uuid) -> reqwest::Result<()> {
+        self.client()
+            .delete(self.format_url(format!("{PATH}/{id}"), None))
+            .bearer_auth(self.bearer_token())
+            .send()
+            .await?
+            .error_for_status()?
+            .json()
+            .await
     }
 }
--- cli/src/args/artifact_action.rs
+++ cli/src/args/artifact_action.rs
@@ -4,6 +4,7 @@ use std::process::ExitCode;
 use clap::Subcommand;
 use log::error;
 use mender_api::{Artifacts, Session};
+use uuid::Uuid;
 
 #[derive(Debug, Subcommand)]
 pub enum ArtifactAction {
@@ -12,6 +13,10 @@ pub enum ArtifactAction {
         page_size: Option<NonZero<usize>>,
         #[clap(long, short = 'v', help = "List detailed device information")]
         verbose: bool,
+    },
+    Delete {
+        #[clap(index = 1, help = "Delete an artifact by its ID")]
+        id: Uuid,
     },
 }
 
@@ -36,6 +41,12 @@ impl ArtifactAction {
                         }
                     }
                 }
+            }
+            Self::Delete { id } => {
+                Artifacts::delete(session, id).await.map_err(|error| {
+                    error!("{error}");
+                    ExitCode::FAILURE
+                })?;
             }
         }
 
